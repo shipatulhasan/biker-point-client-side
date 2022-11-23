90e8aed7959761356f294b160118493b8039e80e
shipatulhasan <shipatulhasan328@gmail.com>
2022-11-23 22:04:17 +0600

    initialization

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <h2 className='text-center text-4xl font-bold'>
+       Welcome to <span className='text-red-200'>biker</span> point
+     </h2>
+    </>
   );
 }
 
